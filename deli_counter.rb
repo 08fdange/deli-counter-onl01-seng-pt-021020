8e1f8973de46e5d327d76ace556a3a2c628625a7
Frank  D'Angelo <08fdange@gmail.com>
2020-02-14 18:18:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  else
+    line_string = "The line is currently: "
+    katz_deli.each.with_index(1) do |person, index|
+      line_string << ("#{index}. #{person} ")
+    end
+    puts line_string
+  end
+end
